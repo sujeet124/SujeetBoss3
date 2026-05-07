93177245b0d673e0bc9060d8f9e8a5b251ab30e9
Sujeet Boss <sujeet8084806524@gmail.com>
2026-05-07 14:34:03 +0530

    Update configuration with new API tokens and IDs
    Updated sensitive configuration values with new tokens and IDs.

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    AUTH_USER = os.environ.get('AUTH_USERS', '').split(',')
+    BOT_TOKEN = os.environ.get("8753755010:AAHjR-nD3AUXKOPzsBxO82eRud7pfzK0jt8")
+    API_ID = int(os.environ.get("28854523"))
+    API_HASH = os.environ.get("3ec8779aa2109cfa00fd6146146a065c")
+    AUTH_USER = os.environ.get('1018181607', '8753755010').split('1018181607')
     AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
     HOST = "https://drm-api-six.vercel.app"
-    CREDIT = "𝗥𝗼𝘆 𝗝𝗮𝗮𝘁™"#Here You Can Change with Your Name  or any custom name or title you prefer
+    CREDIT = "Sujeet™"#Here You Can Change with Your Name  or any custom name or title you prefer
